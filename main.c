4aa4d1d162a7e0e0f1c9713bf9a42c1541e7f0e6
Jacob Smith <jacobsmithsinbox@gmail.com>
2024-02-27 16:17:12 -0700

    [meta] Added gitignore; GitHub CI/CD; CMake; main;

---
+++ main.c
@@ -0,0 +1,22 @@
+/** !
+ * Secret game
+ * 
+ * @file main.c
+ * 
+ * @authors Ben Blodgett, Jacob Smith
+ */
+
+// Header files
+#include <stdio.h>
+#include <stdlib.h>
+
+// Entry point
+int main ( int argc, const char *argv[] )
+{
+
+    // TODO
+    //
+
+    // Success
+    return EXIT_SUCCESS;
+}
